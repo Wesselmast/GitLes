9c82537d7ec7416969bb19ba58e810dfdec3b4a2
Wesselmast <mastwessel@gmail.com>
2018-02-21 22:54:48 +0100

    IT HAS BEGUN

---
+++ Sonar Project/Assets/Trigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger: MonoBehaviour
+{
+    private AudioSource aSource;
+    public AudioClip Fantasy;
+
+    void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" )
+        {
+            aSource.clip = Fantasy;
+            aSource.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            aSource.clip = Fantasy;
+            aSource.Pause();
+        }
+    }
+}
